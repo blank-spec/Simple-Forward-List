415c44b81fa94c3717d7ca24c754a7a0365c7ab5
blank-spec <164775195+blank-spec@users.noreply.github.com>
2024-11-17 19:54:41 +0300

    Update ForwardList.h

--- ForwardList.h
+++ ForwardList.h
@@ -1,36 +1,93 @@
 #pragma once
 #include <iostream>
+
+#include "Vector.h"
 using namespace std;
 
 template <class T>
 class LinkedList {
 private:
-    template<class T>
     struct Node {
         T data;
-        Node<T>* next;
+        Node* next;
 
         Node(const T& value) : data(value), next(nullptr) {}
         Node() : data(T()), next(nullptr) {}
     };
 
-    using TemTemp = Node<T>*;
+    using TemTemp = Node*;
     TemTemp head;
     int size;
 
 public:
+    template <bool isConst>
+    class baseIterator {
+    private:
+        Node* current;
+
+    public:
+        baseIterator(Node* node) : current(node) {}
+        baseIterator() : current(nullptr) {}
+
+        using pointerType = conditional_t<isConst, const T*, T*>;
+        using referenceType = conditional_t<isConst, const T&, T&>;
+        using valueType = T;
+        using difference_type = ptrdiff_t;
+        using iterator_category = random_access_iterator_tag;
+
+        baseIterator(const baseIterator&) = default;
+        baseIterator& operator=(const baseIterator&) = default;
+
+        referenceType operator*() const { return current->data; }
+        pointerType operator->() const { return &current->data; }
+
+        baseIterator& operator++() {
+            current = current->next;
+            return *this;
+        }
+
+        baseIterator operator++(int){
+            baseIterator temp = *this;
+            current = current->next;
+            return temp;
+        }
+
+        bool operator==(const baseIterator& other) const { return current == other.current; }
+        bool operator!=(const baseIterator& other) const { return current != other.current; }
+    };
+
+
+    using Iterator = baseIterator<false>;
+    using constIterator = baseIterator<true>;
+
+    Iterator begin() {
+        return { head };
+    }
+
+    Iterator end() {
+        return { nullptr };
+    }
+
+    constIterator begin() const {
+        return { head };
+    }
+
+    constIterator end() const {
+        return { nullptr };
+    }
+
     LinkedList() : head(nullptr), size(0) {}
 
-    LinkedList(const T& value) : head(new Node<T>(value)), size(1) {}
+    LinkedList(const T& value) : head(new Node(value)), size(1) {}
 
     LinkedList(const LinkedList& other) : head(nullptr), size(other.size) {
         if (other.head) {
-            head = new Node<T>(other.head->data);
+            head = new Node(other.head->data);
             TemTemp temp = head;
             TemTemp otherTemp = other.head->next;
 
-            while (otherCurrent) {
-                temp->next = new Node<T>(otherTemp->data);
+            while (otherTemp) {
+                temp->next = new Node(otherTemp->data);
                 temp = temp->next;
                 otherTemp = otherTemp->next;
             }
@@ -44,12 +101,12 @@ public:
             size = other.size;
 
             if (other.head) {
-                head = new Node<T>(other.head->data);
+                head = new Node(other.head->data);
                 TemTemp temp = head;
                 TemTemp otherTemp = other.head->next;
 
                 while (otherTemp) {
-                    temp->next = new Node<T>(otherTemp->data);
+                    temp->next = new Node(otherTemp->data);
                     temp = temp->next;
                     otherTemp = otherTemp->next;
                 }
@@ -105,14 +162,14 @@ public:
     }
 
     void remove(const int index) {
-        if (!head || index < 0 || index > size) 
+        if (!head || index < 0 || index > size)
             throw out_of_range("Index out of bound");
 
         TemTemp temp = head;
         --size;
         if (index == 0) {
             head = head->next;
-            delete temp; 
+            delete temp;
             return;
         }
 
@@ -120,23 +177,25 @@ public:
             temp = temp->next;
         }
 
-
         if (temp != nullptr && temp->next != nullptr) {
             TemTemp nodeToDelete = temp->next;
             temp->next = nodeToDelete->next;
             delete nodeToDelete;
+            cout << "Deleted" << endl;
         }
     }
 
-    void pop_front()                 { remove(0); }
+    void pop_front() { remove(0); }
 
-    void pop_back()                  { remove(size - 1); }
+    void pop_back() { remove(size - 1); }
 
-    void push_front(const T& value)  { insert(0, value); }
+    void push_front(const T& value) { insert(0, value); }
 
-    void push_back(const T& value)   { insert(size, value); }
+    void push_back(const T& value) { insert(size, value); }
 
-    int& at(const int index) const {
+    bool empty() const { return head == nullptr; }
+
+    [[nodiscard]] int& at(const int index) const {
         if (index < 0 || index > size) {
             throw out_of_range("Index out of bound");
         }
@@ -167,7 +226,7 @@ public:
         TemTemp temp = head;
 
         int count = 0;
-        
+
         while (temp) {
             if (temp->data == value)
                 return count;
@@ -176,6 +235,23 @@ public:
         }
 
         return size;
+    }
+
+    T& front() const  {
+        return head->data;
+    }
+
+    T& back() const {
+        if (!head) {
+            throw std::runtime_error("List is empty");
+        }
+
+        Node* node = head;
+
+        while (node->next) {
+            node = node->next;
+        }
+        return node->data;
     }
 
     bool find(const T& value) const {
@@ -198,7 +274,7 @@ public:
         cout << "nullptr" << endl;
     }
 
-    int getSize() const { return size; }
+    [[nodiscard]] int getSize() const { return size; }
 
     ~LinkedList() {
         if (!head)
